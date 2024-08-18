1d0e3949a4d2801ac225307e17d30f6bb8532dda
HARUN360 <mdharunorroshid301204@gmail.com>
2024-08-18 17:52:07 +0600

    this is cors police state

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://super-shop-6acda.web.app"
        
     ],
     credentials: true,
