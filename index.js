7541226da2199a7d2a974fb890012342a4c44fb9
HARUN360 <mdharunorroshid301204@gmail.com>
2024-08-18 13:14:41 +0600

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        // "https://megastore54.netlify.app"
+       
     ],
     credentials: true,
 }
